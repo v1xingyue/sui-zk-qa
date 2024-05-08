91103532dc076b02f1de8210e80939a242f0172f
v1xingyue <qixingyue@gmail.com>
2024-05-08 10:24:36 +0800

    modify zklogin

--- src/main.tsx
+++ src/main.tsx
@@ -12,11 +12,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@mysten/dapp-kit/dist/index.css";
 import { registerZkSendWallet } from "@mysten/zksend";
 
-registerZkSendWallet("zkqa", { origin: "https://zksend.com/" });
+registerZkSendWallet("zkQA", { origin: "https://zksend.com" });
 
 const queryClient = new QueryClient();
 
 const { networkConfig } = createNetworkConfig({
+  // devnet: { url: getFullnodeUrl("devnet") },
+  // testnet: { url: getFullnodeUrl("testnet") },
   mainnet: { url: getFullnodeUrl("mainnet") },
 });
 
@@ -24,7 +26,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
-        <WalletProvider autoConnect={true}>
+        <WalletProvider autoConnect>
           <App />
         </WalletProvider>
       </SuiClientProvider>
